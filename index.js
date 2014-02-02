a54aa22769a8411769f828fbc110eb203f6301a9
Chris Anderson <chris.anderson@my.wheaton.edu>
2014-02-02 00:24:43 -0600

    Another offset bug fix

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ var getOffset = function(el) {
     var _x = 0;
     var _y = 0;
     while (el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
-        _x += el.offsetLeft - el.scrollLeft;
-        _y += el.offsetTop - el.scrollTop;
+        _x += el.offsetLeft;
+        _y += el.offsetTop;
         el = el.offsetParent;
     }
     return { top: _y, left: _x };
